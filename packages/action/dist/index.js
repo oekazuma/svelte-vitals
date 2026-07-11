833a9de1879b27accefc0a63f1d2d01b3ea0472e
oekazuma <koneko0604@gmail.com>
2026-07-12 02:08:30 +0900

    chore(action): rebuild dist/ to pick up the wave/Health alignment fix

--- packages/action/dist/index.js
+++ packages/action/dist/index.js
@@ -56976,7 +56976,7 @@ function applyRuleSeverities(results, config) {
   });
 }
 
-// ../cli/dist/chunk-Y4R7TIS6.js
+// ../cli/dist/chunk-72V4NCNC.js
 import { readFile, access as access2 } from "fs/promises";
 import { join } from "path";
 
@@ -57754,7 +57754,7 @@ async function glob(globInput, options) {
   return crawler ? formatPaths(await crawler.withPromise(), relative2) : [];
 }
 
-// ../cli/dist/chunk-Y4R7TIS6.js
+// ../cli/dist/chunk-72V4NCNC.js
 import { readFileSync as readFileSync2 } from "fs";
 import { execFileSync } from "child_process";
 import { execFileSync as execFileSync2 } from "child_process";
