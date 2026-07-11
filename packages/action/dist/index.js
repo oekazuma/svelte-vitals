70bf18d6e05e352a5f81bce2c2f4ea134cd2b897
oekazuma <koneko0604@gmail.com>
2026-07-11 22:09:54 +0900

    chore(action): rebuild dist/ to pick up the mouth-centering fix

--- packages/action/dist/index.js
+++ packages/action/dist/index.js
@@ -56976,7 +56976,7 @@ function applyRuleSeverities(results, config) {
   });
 }
 
-// ../cli/dist/chunk-PP6VVGHF.js
+// ../cli/dist/chunk-5UB626JW.js
 import { readFile, access as access2 } from "fs/promises";
 import { join } from "path";
 
@@ -57754,7 +57754,7 @@ async function glob(globInput, options) {
   return crawler ? formatPaths(await crawler.withPromise(), relative2) : [];
 }
 
-// ../cli/dist/chunk-PP6VVGHF.js
+// ../cli/dist/chunk-5UB626JW.js
 import { readFileSync as readFileSync2 } from "fs";
 import { execFileSync } from "child_process";
 import { execFileSync as execFileSync2 } from "child_process";
