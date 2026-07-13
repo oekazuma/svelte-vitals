02eaf176de86ee98fc0289672ce75523d8794c5e
oekazuma <oe.kazuma@a-tm.co.jp>
2026-07-13 18:20:44 +0900

    chore(action): rebuild dist/ after exporting main() for tests

--- packages/action/dist/index.js
+++ packages/action/dist/index.js
@@ -58754,6 +58754,9 @@ ${markdown}`;
 main().catch((err) => {
   setFailed(err instanceof Error ? err.message : String(err));
 });
+export {
+  main
+};
 /*! Bundled license information:
 
 undici/lib/web/fetch/body.js:
